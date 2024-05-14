de9b000932bf74f28f5eb078fed537f071f44674
Diego Valcarce <dvalcarcer@gmail.com>
2024-05-14 10:27:05 +0200

    Debug

--- src/action-configuration.ts
+++ src/action-configuration.ts
@@ -188,8 +188,12 @@ function getSourceCodeConfig(): ICodeConfiguration {
 }
 
 function getRuntime(): Runtime {
+    console.log("Before getting runtime");
     const rawRuntime = getInput('runtime', { required: true });
+    console.log("Got the runtime ", rawRuntime);
     const runtime = rawRuntime.toUpperCase();
+    console.log("Got the runtime uppercase ", runtime);
+    console.log("Valid runtimes", JSON.stringify(Object.keys(Runtime)));
     if (!Object.keys(Runtime).includes(runtime)) {
         throw new Error(`Specified runtime (${rawRuntime}) does not belong to the supported range: ${JSON.stringify(Object.keys(Runtime))}`);
     }
